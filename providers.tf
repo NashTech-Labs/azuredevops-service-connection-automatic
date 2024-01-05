7552b8feaa3605bb6ce89fbc59a64a9cc4e5bdd4
shivamgupta-sg <76810109+shivamgupta-sg@users.noreply.github.com>
2024-01-05 13:06:49 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -16,6 +16,6 @@ provider "azurerm" {
 }
 
 provider "azuredevops" {
-  org_service_url       = var.org_service_url
-  personal_access_token = var.personal_access_token
+  org_service_url       = var.azuredevops_org_service_url
+  personal_access_token = var.azuredevops_personal_access_token
 }
